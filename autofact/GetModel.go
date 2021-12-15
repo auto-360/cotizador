57d23456bab1f49fbf3b5441d08cd4a4e04e7b17
Manuel Alba <malba@mmae.cl>
2021-12-14 21:21:23 -0300

    FIX

---
+++ autofact/GetModel.go
@@ -0,0 +1,37 @@
+package autofact
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func GetModels(patente string) {
+
+	url := URL + "/v1/versiones/?patente=" + patente
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+TOKEN)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
